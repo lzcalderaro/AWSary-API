1d733e975ac6d7e9b2d6d4078144a042ae4743e6
Luiz Calderaro <lzcalderaro@gmail.com>
2024-04-20 15:52:15 +0100

    Update Docker file

--- src/main/kotlin/com/awsary/plugins/Routing.kt
+++ src/main/kotlin/com/awsary/plugins/Routing.kt
@@ -2,16 +2,13 @@ package com.awsary.plugins
 
 import com.awsary.client.Database
 import io.ktor.http.*
-import io.ktor.resources.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.openapi.*
-import io.ktor.server.resources.*
 import io.ktor.server.resources.Resources
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
     install(ContentNegotiation) {
@@ -69,5 +66,4 @@ fun Route.awsItemsRouting() {
             }
         }
     }
-
 }
